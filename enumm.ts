6c294413374cfb9d637ad7682111277f626011c6
Ragulraj <124597431+Ragulraj005@users.noreply.github.com>
2024-01-18 13:09:56 +0530

    Create enumm.ts

---
+++ enumm.ts
@@ -0,0 +1,17 @@
+// Enum declaration
+enum Direction {
+  Up = "UP",
+  Down = "DOWN",
+  Left = "LEFT",
+  Right = "RIGHT",
+}
+
+// Using the enum
+let userDirection: Direction = Direction.Up;
+
+// Switch statement with enum
+switch (userDirection) {
+  case Direction.Up:
+    console.log("Moving Up");
+    break;
+  case Direction.Down:
