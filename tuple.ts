59f6bba70c7be1d1c280abbeb888a199562aa1f1
Ragulraj <124597431+Ragulraj005@users.noreply.github.com>
2024-01-31 15:13:38 +0530

    Create tuple.ts

---
+++ tuple.ts
@@ -0,0 +1,10 @@
+// Declare a tuple type
+let myTuple: [string, number, boolean];
+
+// Initialize the tuple
+myTuple = ["Hello", 42, true];
+
+// Access elements by index
+const firstElement: string = myTuple[0];
+const secondElement: number = myTuple[1];
+const thirdElement: boolean = myTuple[2];
